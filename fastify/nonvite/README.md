5b671d8dc5a46e4c6b2c29cd8bc8616332bb38d0
jonathanroll <75393433+jonathanroll@users.noreply.github.com>
2024-01-08 21:00:07 +0000

    fastify experiments

---
+++ fastify/nonvite/README.md
@@ -0,0 +1,3 @@
+```sh
+npx create-remix@latest --template https://github.com/mcansh/remix-fastify/tree/main/examples/basic
+```
---
+++ kudos-remix-raw/app/root.tsx
@@ -0,0 +1,40 @@
+import type { LoaderArgs } from "@remix-run/node";
+import { json, redirect } from "@remix-run/node";
+import {
+  Form,
+  Links,
+  LiveReload,
+  Meta,
+  NavLink,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+  useLoaderData,
+  useNavigation,
+  useSubmit,
+} from "@remix-run/react";
+import { useEffect } from "react";
+
+import { createEmptyContact, getContacts } from "./data";
+
+import "./styles/app.css";
+// import "./tailwind.css";
+
+export default function App() {
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        <Outlet />
+        <ScrollRestoration />
+        <Scripts />
+        <LiveReload />
+      </body>
+    </html>
+  );
+}
